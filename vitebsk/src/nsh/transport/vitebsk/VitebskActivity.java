426e65ed2a33ad29ee021a515cad41b57757711b
n-sh <n.sh4dev@gmail.com>
2012-09-28 18:22:07 +0300

    initial project add

---
+++ vitebsk/src/nsh/transport/vitebsk/VitebskActivity.java
@@ -0,0 +1,13 @@
+package nsh.transport.vitebsk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VitebskActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
